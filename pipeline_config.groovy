35b4a0883adcce91393d15fe2ae0aefac4811827
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-09-14 18:10:44 +0300

    sonarqube

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -7,6 +7,9 @@ libraries {
     git {
         repo_url = "https://github.com/livelace/digator-label-studio.git"
     }
+    harbor_replicate {
+        policy = "digator-label-studio"
+    }
     k8s_build
     kaniko {
         source = "digator_opennlp.dockerfile"
