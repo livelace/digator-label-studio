68acda81bad605bbbe678c1c59f7edf290260c1d
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-10-03 17:08:06 +0300

    ci

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,19 +1,24 @@
+def APP_NAME = "digator-label-studio"
+def APP_REPO = "https://github.com/livelace/digator-label-studio.git"
+def APP_VERSION = env.VERSION + '-${GIT_COMMIT_SHORT}'
+
 libraries {
     dependency_check
     dependency_track {
-        project = "digator-label-studio"
-        version = "master"
+        project = "${APP_NAME}"
+        version = env.VERSION
     }
     git {
-        repo_url = "https://github.com/livelace/digator-label-studio.git"
+        repo_url = "${APP_REPO}"
+        repo_branch = env.VERSION
     }
     harbor_replicate {
-        policy = "digator-label-studio"
+        policy = "${APP_NAME}"
     }
     k8s_build
     kaniko {
-        source = "digator_opennlp.dockerfile"
-        destination = "data/digator-label-studio:digator_opennlp"
+        source = "${env.BACKEND}.dockerfile"
+        destination = "data/${APP_NAME}:${env.BACKEND}"
     }
     mattermost
     python
