4acb4e870b0fab36a66d9dafe2a2270bc21618a4
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-10-03 17:18:44 +0300

    ci

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,16 +1,15 @@
 def APP_NAME = "digator-label-studio"
 def APP_REPO = "https://github.com/livelace/digator-label-studio.git"
-def APP_VERSION = env.VERSION + '-${GIT_COMMIT_SHORT}'
 
 libraries {
     dependency_check
     dependency_track {
         project = "${APP_NAME}"
-        version = "env.VERSION"
+        version = env.VERSION
     }
     git {
         repo_url = "${APP_REPO}"
-        //repo_branch = env.VERSION
+        repo_branch = env.VERSION
     }
     harbor_replicate {
         policy = "${APP_NAME}"
